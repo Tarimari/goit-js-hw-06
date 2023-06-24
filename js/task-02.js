bf3d540fcc7b5fb9b9daa877b810d3a842f5235d
Tarimari <119706474+Tarimari@users.noreply.github.com>
2023-06-24 13:57:45 +0300

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingredient = document.querySelector("#ingredients");
+let newElements = [];
+
+ingredients.forEach(ingr => {
+  const li = document.createElement("li");
+  li.textContent = ingr;
+  li.classList.add("item");
+  newElements.push(li);
+})
+ingredient.append(...newElements);
