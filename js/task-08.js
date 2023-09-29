8e7ad6971b82e360519f59bd28c0bf97b5365f51
Tarimari <tarashorban1987@gmail.com>
2023-09-30 01:08:10 +0300

    3009

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(`.login-form`)
+const inpMail = form.firstElementChild.lastElementChild;
+const inpPass = form.firstElementChild.nextElementSibling.lastElementChild;
+const formBtn = form.lastElementChild;
 
+form.addEventListener(`submit`, handlerSubmit);
+
+
+function handlerSubmit(evt) {
+    evt.preventDefault();
+    if (!inpMail.value || !inpPass.value) {
+        alert(`Попередження про те, що всі поля повинні бути заповнені.`);
+        return;
+    }
+    const {email, password} = evt.currentTarget.elements;
+    const result = {
+        email: email.value,
+        password: password.value,
+    }
+    console.dir(result);
+    evt.currentTarget.reset();
+}
