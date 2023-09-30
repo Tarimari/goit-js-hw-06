666cb1e47877bc4255fdad783b43e25bfbb7bc4d
Tarimari <tarashorban1987@gmail.com>
2023-10-01 00:14:50 +0300

    0110

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const body = document.querySelector(`body`);
+const btn = document.querySelector(`.change-color`);
+const bgColorCod = document.querySelector(`.color`);
+
+btn.addEventListener(`click`, hendlerClick);
+
+function hendlerClick() {
+  bgColorCod.textContent = getRandomHexColor();
+  body.style.backgroundColor = getRandomHexColor();
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
