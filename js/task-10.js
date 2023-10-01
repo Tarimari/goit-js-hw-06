7f585f7292fd8d6a12c598640021f3bd2a8330f8
Tarimari <tarashorban1987@gmail.com>
2023-10-01 18:41:31 +0300

    0110

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,43 @@
+const form = document.querySelector(`#controls`);
+const inp = document.querySelector(`input`);
+const createBtn = inp.nextElementSibling;
+const destroyBtn = createBtn.nextElementSibling;
+const box = document.querySelector(`#boxes`);
+
+createBtn.addEventListener(`click`, hendlerClick);
+destroyBtn.addEventListener(`click`, destroyBoxes);
+
+function hendlerClick() {
+  createBoxes(inp.value);
+}
+
+function createBoxes(amount) {
+  const arr = [];
+  let lastId = 0;
+  if (!box.lastElementChild) { lastId = 0 }
+  else { lastId = Number(box.lastElementChild.id) };
+
+  for (let i = 1; i <= amount; i++) {
+    arr.push(`<div class="boxes" id="${i + lastId}"></div>`)
+  };
+console.dir(typeof(arr.join("")));
+
+  box.insertAdjacentHTML("beforeend", arr.join(""));
+  const cubs = document.querySelectorAll(`.boxes`);
+
+  cubs.forEach((cub) => {
+    cub.style.width = `${Number(cub.id) + 10}px`;
+    cub.style.height = `${Number(cub.id) + 10}px`;
+    cub.style.backgroundColor = getRandomHexColor();
+  });
+  console.dir(document.querySelectorAll(`.boxes`));
+}
+
+function destroyBoxes() {
+  box.innerHTML = "";
+}
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
